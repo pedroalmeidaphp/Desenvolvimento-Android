2d78e5567a2a61ff102a87453513635b17b8733a
Pedro Henrique <103169173+pedroalmeidaphp@users.noreply.github.com>
2023-08-20 16:25:37 -0300

    ajeitando layout

--- ListaVip/app/src/main/java/com/pedro/listavip/model/Pessoa.java
+++ ListaVip/app/src/main/java/com/pedro/listavip/model/Pessoa.java
@@ -1,7 +1,57 @@
 package com.pedro.listavip.model;
 
 public class Pessoa {
+    //ATRIBUTES
+    private String firstName;
+    private String lastName;
+    private String course;
+    private String contact;
+
+
+    //constructor
     public Pessoa(){
 
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", course='" + course + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
     }
 }
--- ListaVip/app/src/main/java/com/pedro/listavip/view/MainActivity.java
+++ ListaVip/app/src/main/java/com/pedro/listavip/view/MainActivity.java
@@ -3,16 +3,24 @@ package com.pedro.listavip.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.pedro.listavip.R;
 import com.pedro.listavip.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
+    String dados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pessoa= new Pessoa();
+        pessoa.setFirstName("Pedro");
+        pessoa.setLastName("Almeida");
+        pessoa.setCourse("CC");
+        pessoa.setContact("16 993697830");
+        dados = pessoa.getFirstName();
+        Log.i("poo", pessoa.toString());
     }
 }
