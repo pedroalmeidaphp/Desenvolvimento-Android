ed360566d8b59583430e25414cf11adb5df397f3
Pedro Henrique <103169173+pedroalmeidaphp@users.noreply.github.com>
2023-08-27 17:27:26 -0300

    resolvendo bug de instanciamento do objeto pessoa, e criação de layout para spinner

--- ListaVip/app/src/main/java/com/pedro/listavip/view/MainActivity.java
+++ ListaVip/app/src/main/java/com/pedro/listavip/view/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pessoa= new Pessoa();
 
         controller= new PessoaController(MainActivity.this);
         controller.buscar(pessoa);
