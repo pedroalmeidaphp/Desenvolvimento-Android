2832773d2c085cefd675d5810f62d35aa4568043
Pedro Henrique <103169173+pedroalmeidaphp@users.noreply.github.com>
2023-08-30 11:50:49 -0300

    layout ajeitado

--- ListaVip/app/src/main/java/com/pedro/listavip/view/MainActivity.java
+++ ListaVip/app/src/main/java/com/pedro/listavip/view/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spinner);
         pessoa= new Pessoa();
 
         controller= new PessoaController(MainActivity.this);
